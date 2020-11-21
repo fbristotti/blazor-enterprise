b8dee30275090792baffc973192955cb9d155df8
Fernando Bristotti <fbristotti@gmail.com>
2020-11-21 09:27:26 -0300

    refactoring code using dependency injection

---
+++ BethanysPieShopHRM.UI/Services/ExpenseApprovalService.cs
@@ -0,0 +1,61 @@
+﻿using BethanysPieShopHRM.Shared;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.UI.Services
+{
+    public class ExpenseApprovalService : IExpenseApprovalService
+    {
+        private readonly IEmployeeDataService _employeeService;
+
+        public ExpenseApprovalService(IEmployeeDataService employeeService)
+        {
+            _employeeService = employeeService ?? throw new System.ArgumentNullException(nameof(employeeService));
+        }
+
+        public async Task<ExpenseStatus> GetExpenseStatus(Expense expense)
+        {
+            var employee = await _employeeService.GetEmployeeDetails(expense.EmployeeId);
+
+            
+
+            // We can handle certain requests automatically
+            if (employee.IsOPEX)
+            {
+                switch (expense.ExpenseType)
+                {
+                    case ExpenseType.Conference:
+                        return ExpenseStatus.Denied;
+                    case ExpenseType.Transportation:
+                        return ExpenseStatus.Denied;
+                    case ExpenseType.Hotel:
+                        return ExpenseStatus.Denied;
+                }
+
+                if (expense.Status != ExpenseStatus.Denied)
+                {
+                    expense.CoveredAmount = expense.Amount / 2;
+                }
+            }
+
+            if (!employee.IsFTE)
+            {
+                if (expense.ExpenseType != ExpenseType.Training)
+                {
+                    return ExpenseStatus.Denied;
+                }
+            }
+
+            if (expense.ExpenseType == ExpenseType.Food && expense.Amount > 100)
+            {
+                return ExpenseStatus.Pending;
+            }
+
+            if (expense.Amount > 5000)
+            {
+                return ExpenseStatus.Pending;
+            }
+
+            return ExpenseStatus.Pending;
+        }
+    }
+}
